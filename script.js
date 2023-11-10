1f5ac0851e588396338d90d8869a2d5fea4df053
HasanKazemi <avatarhasan78@gmail.com>
2023-11-10 14:33:40 +0330

    Date and Time Countdown

--- script.js
+++ script.js
@@ -1,13 +1,39 @@
-let countdown = document.querySelector('.countdown')
+// catch the Countdown Date elements
+const CountdownDay = document.querySelector("#day")
+const CountdownMonth = document.querySelector("#month")
+const CountdownYear = document.querySelector("#year")
+const CountdownHour = document.querySelector("#hour")
+const CountdownMinute = document.querySelector("#minute")
+const CountdownSecond = document.querySelector("#second")
 
-document.querySelector('.start-countdown').addEventListener("click",()=>{
-    let number = document.querySelector('#number').value
-    countdown.innerHTML = number
-    setInterval(function() {
-        if (number === 0){
-            return
-        }else {
-            countdown.innerHTML = number -= 1
+document.querySelector(".start-countdown").addEventListener("click", ()=>{
+    // get month index by minus from month number
+    let monthIndex = CountdownMonth.value - 1
+    // set Countdown Date
+    let countdownDate = new Date(CountdownYear.value, monthIndex, CountdownDay.value, CountdownHour.value, CountdownMinute.value, CountdownSecond.value)
+
+    // update Countdown every second
+    let time = setInterval(() => {
+        // get the current date and time
+        let now = new Date().getTime()
+        // Calculate the time remaining until the countdown date
+        let distance = countdownDate - now
+
+        // Calculate the days, hours, minutes, and seconds remaining
+        let days = Math.floor(distance / (1000 * 60 * 60 * 24));
+        let hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))
+        let minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60))
+        let seconds = Math.floor((distance % (1000 * 60)) / 1000)
+    
+        // display the countdown
+        const showCountdown = document.querySelector(".countdown")
+        showCountdown.innerHTML = `${days}d ${hours}:${minutes}':${seconds}"`
+        showCountdown.innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s "
+
+        // if the countdown is over, display a message
+        if (distance < 0) {
+            clearInterval(time)
+            showCountdown.innerHTML = "Expired"
         }
-    }, 1000)
+    }, 1000);
 })
